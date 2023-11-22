dd640f0c074e4ce15da2103c38cd5064149aee81
Pasindu Vinsuka <88902687+pasinduvinsuka@users.noreply.github.com>
2023-11-22 15:53:06 +0530

    finish chapter 9

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,19 +1,17 @@
-import { Card } from '@/app/ui/dashboard/cards';
-import RevenueChart from '@/app/ui/dashboard/revenue-chart';
-import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
-import { lusitana } from '@/app/ui/fonts';
-import { fetchRevenue, fetchLatestInvoices,fetchCardData } from '../lib/data';
- 
-export default async function Page() {
-    const revenue = await fetchRevenue();
-    const latestInvoices = await fetchLatestInvoices();
+import RevenueChart from "@/app/ui/dashboard/revenue-chart";
+import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
+import { lusitana } from "@/app/ui/fonts";
+import { Suspense } from "react";
+import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from "@/app/ui/skeletons";
+import CardWrapper from "@/app/ui/dashboard/cards";
 
+export default async function Page() {
   // const totalPaidInvoices = await (await fetchCardData()).totalPaidInvoices;
   // const totalPendingInvoices = await (await fetchCardData()).totalPendingInvoices;
   // const numberOfInvoices = await (await fetchCardData()).numberOfInvoices;
   // const numberOfCustomers = await (await fetchCardData()).numberOfCustomers;
 
-/*
+  /*
 ## above first approach :
 in the above approach we are  repeatedly calling the 'await fetchDataCard()' for each property we need to extract.This means that we are fetching the data multiple times.
 instead of calling like above we can follow the below appproach.Each call to await fetchCardData() initiates a new asynchronous operation, and these operations are not running concurrently. This can be less efficient, especially if fetchCardData() is an expensive operation, as you are making redundant calls.
@@ -22,14 +20,12 @@ instead of calling like above we can follow the below appproach.Each call to awa
 In this approach, you are destructuring the result of await fetchCardData() in a single step. This means that you are fetching the data only once, and then extracting the properties you need in a single operation. This is generally more efficient and concise.
 
 */
-const {
-  totalPaidInvoices,
-  totalPendingInvoices,
-  numberOfCustomers,
-  numberOfInvoices
-} =  await fetchCardData();
-
-
+  // const {
+  //   totalPaidInvoices,
+  //   totalPendingInvoices,
+  //   numberOfCustomers,
+  //   numberOfInvoices,
+  // } = await fetchCardData();
 
   return (
     <main>
@@ -37,18 +33,17 @@ const {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <Card title="Collected" value={totalPaidInvoices} type="collected" />
-        <Card title="Pending" value={totalPendingInvoices} type="pending" />
-        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-        <Card
-          title="Total Customers"
-          value={numberOfCustomers}
-          type="customers"
-        />
+      <Suspense fallback = {<CardsSkeleton/>}>
+        <CardWrapper/>
+      </Suspense>
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <RevenueChart revenue={revenue}  />
-        <LatestInvoices latestInvoices={latestInvoices} />
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
+        <Suspense fallback = {<LatestInvoicesSkeleton/>}>
+          <LatestInvoices />
+        </Suspense>
       </div>
     </main>
   );
